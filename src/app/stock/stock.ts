fa1abe3b6545b150c9f83d1256bde4bc5440812a
Marcio Sansone <msansone@gmail.com>
2025-08-25 15:52:23 +0100

    Task#12

--- src/app/stock/stock.ts
+++ src/app/stock/stock.ts
@@ -1,20 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms'; // Import FormsModule
-import { Stock, Transaction, Provent } from './stock.model';
+import { Stock, StockDetail } from './stock.model';
 import { StockService } from './stock.service';
 
 @Component({
   selector: 'app-stock',
   imports: [CommonModule, FormsModule], // Add FormsModule here
   templateUrl: './stock.html',
-  
+  styleUrls: ['./stock.css']
 })
 export class StockComponent implements OnInit {
   stocks: Stock[] = [];
-  selectedStock: Stock | undefined;
+  selectedStockTick: string | undefined;
+  selectedStockDetail: StockDetail | undefined;
 
-  constructor(private stockService: StockService) { }
+  constructor(private stockService: StockService, private cdr: ChangeDetectorRef) { }
 
   ngOnInit(): void {
     this.loadAllStocks();
@@ -22,29 +23,19 @@ export class StockComponent implements OnInit {
 
   loadAllStocks(): void {
     this.stockService.getStocks().subscribe(data => {
-      console.log('API Data:', data);
-      this.stocks = data.map(item => {
-        return {
-          tick: item.tick,
-          company: item.company,
-          quantidadeAcoes: 100,
-          totalInvestido: 10000,
-          valorMedio: 100,
-          valorTotalProventos: 500,
-          proventosUltimos12Meses: 200,
-          mediaUltimos4Meses: 50,
-          transactions: [
-            { data: new Date(), valor: 100, quantidade: 10, total: 1000 },
-            { data: new Date(), valor: 105, quantidade: 5, total: 525 },
-          ],
-          provents: [
-            { data: new Date(), valor: 10, quantidade: 100, total: 1000 },
-            { data: new Date(), valor: 12, quantidade: 100, total: 1200 },
-          ]
-        };
-      });
-      console.log('Mapped Stocks:', this.stocks);
+      this.stocks = data;
     });
   }
+
+  onStockSelected(): void {
+    if (this.selectedStockTick) {
+      this.stockService.getStockDetailByTick(this.selectedStockTick).subscribe(data => {
+        console.log('Stock detail received:', data);
+        this.selectedStockDetail = data;
+        this.cdr.detectChanges();
+      });
+    }
+  }
 }
+
 
