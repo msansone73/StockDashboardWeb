0dd1eb3418e339583c062fd385151ae57963e34e
Marcio Sansone <msansone@gmail.com>
2025-08-25 11:21:04 +0100

    task#10 -

--- src/app/stock/stock.model.ts
+++ src/app/stock/stock.model.ts
@@ -1,4 +1,26 @@
+export interface Transaction {
+  data: Date;
+  valor: number;
+  quantidade: number;
+  total: number;
+}
+
+export interface Provent {
+  data: Date;
+  valor: number;
+  quantidade: number;
+  total: number;
+}
+
 export interface Stock {
   tick: string;
   company: string;
+  quantidadeAcoes: number;
+  totalInvestido: number;
+  valorMedio: number;
+  valorTotalProventos: number;
+  proventosUltimos12Meses: number;
+  mediaUltimos4Meses: number;
+  transactions: Transaction[];
+  provents: Provent[];
 }
